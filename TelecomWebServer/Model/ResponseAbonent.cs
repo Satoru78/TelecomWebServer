0a767c8de228e16de179ebef5cda9285717ceee0
kira <kira@mail.ru>
2023-01-03 22:55:53 +0300

    Добавьте файлы проекта.

---
+++ TelecomWebServer/Model/ResponseAbonent.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TelecomWebServer.Model
+{
+    class ResponseAbonent
+    {
+        public ResponseAbonent(Abonents abonents)
+        {
+            this.NumberAbonent = abonents.NumberAbonent;
+            this.FIO = abonents.FIO;
+            this.IDGender = abonents.IDGender;
+            this.DateOfBirth = abonents.DateOfBirth;
+            this.Phone = abonents.Phone;
+            this.Email = abonents.Email;
+            this.AdressPropiski = abonents.AdressPropiski;
+            this.Adress = abonents.Adress;
+            this.SerialAndNumber = abonents.SerialAndNumber;
+            this.Code = abonents.Code;
+            this.Issue = abonents.Issue;
+            this.DateOfIssue = abonents.DateOfIssue;
+            this.NumberOfDogovor = abonents.NumberOfDogovor;
+            this.DateOfStart = abonents.DateOfStart;
+            this.IDTypeOfDogovor = abonents.IDTypeOfDogovor;
+            this.Reason = abonents.Reason;
+            this.PersonalAccount = abonents.PersonalAccount;
+            this.Services = abonents.Services;
+            this.DateOfEnd = abonents.DateOfEnd;
+            this.IDAbonentEqupment = abonents.IDAbonentEqupment;
+
+
+        }
+        public ResponseAbonent() { }
+        public string NumberAbonent { get; set; }
+        public string FIO { get; set; }
+        public int IDGender { get; set; }
+        public System.DateTime DateOfBirth { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public string AdressPropiski { get; set; }
+        public string Adress { get; set; }
+        public string SerialAndNumber { get; set; }
+        public string Code { get; set; }
+        public string Issue { get; set; }
+        public System.DateTime DateOfIssue { get; set; }
+        public string NumberOfDogovor { get; set; }
+        public System.DateTime DateOfStart { get; set; }
+        public int IDTypeOfDogovor { get; set; }
+        public string Reason { get; set; }
+        public int PersonalAccount { get; set; }
+        public string Services { get; set; }
+        public Nullable<System.DateTime> DateOfEnd { get; set; }
+        public string IDAbonentEqupment { get; set; }
+    }
+}
---
+++ TelecomWebServer/Program.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.Encodings.Web;
+using System.Text.Json;
+using System.Text.Unicode;
+using System.Threading.Tasks;
+using TelecomWebServer.Context;
+using TelecomWebServer.Model;
+
+
+namespace TelecomWebServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HttpListener server = new HttpListener();
+            server.Prefixes.Add("http://localhost:15300/");
+
+            JsonSerializerOptions options = new JsonSerializerOptions() { Encoder = JavaScriptEncoder.Create(UnicodeRanges.All) };
+            server.Start();
+
+            while (true)
+            {
+                HttpListenerContext context = server.GetContext();
+                if (context.Request.HttpMethod == "GET")
+                {
+                    try
+                    {
+                        if (context.Request.RawUrl == "/api/abonents/")
+                        {
+                            var abonentList = Data.tns.Abonents.ToList();
+                            string response = JsonSerializer.Serialize(Data.tns.Abonents.ToList().ConvertAll(b => new ResponseAbonent(b)), options);
+                            byte[] data = Encoding.UTF8.GetBytes(response);
+                            context.Response.ContentType = "application/json;charset=utf-8";
+                            using (Stream stream = context.Response.OutputStream)
+                            {
+                                stream.Write(data, 0, data.Length);
+                                context.Response.StatusCode = 200;
+                            }
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                        context.Response.StatusCode = 400;
+                        context.Response.Close();
+
+                    }
+                }
+                else if (context.Request.HttpMethod == "POST")
+                {
+                    try
+                    {
+                        if (context.Request.RawUrl == "/api/abonents/")
+                        {
+                            if (context.Request.ContentType == "application/json")
+                            {
+                                string request = "";
+                                byte[] data = new byte[context.Request.ContentLength64];
+                                using (Stream stream = context.Request.InputStream)
+                                {
+                                    stream.Read(data, 0, data.Length);
+                                }
+                                request = UTF8Encoding.UTF8.GetString(data);
+                                var abonentList = JsonSerializer.Deserialize<List<ResponseAbonent>>(request);
+                                foreach (var abonent in abonentList)
+                                {
+                                    Abonents item = new Abonents();
+                                    item.NumberAbonent = abonent.NumberAbonent;
+                                    item.FIO = abonent.FIO;
+                                    item.IDGender = abonent.IDGender;
+                                    item.DateOfBirth = abonent.DateOfBirth;
+                                    item.Phone = abonent.Phone;
+                                    item.Email = abonent.Email;
+                                    item.AdressPropiski = abonent.AdressPropiski;
+                                    item.Adress = abonent.Adress;
+                                    item.SerialAndNumber = abonent.SerialAndNumber;
+                                    item.Code = abonent.Code;
+                                    item.Issue = abonent.Issue;
+                                    item.DateOfIssue = abonent.DateOfIssue;
+                                    item.NumberOfDogovor = abonent.NumberOfDogovor;
+                                    item.DateOfStart = abonent.DateOfStart;
+                                    item.IDTypeOfDogovor = abonent.IDTypeOfDogovor;
+                                    item.Reason = abonent.Reason;
+                                    item.PersonalAccount = abonent.PersonalAccount;
+                                    item.Services = abonent.Services;
+                                    item.DateOfEnd = abonent.DateOfEnd;
+                                    item.IDAbonentEqupment = abonent.IDAbonentEqupment;
+                                    Data.tns.Abonents.Add(item);
+                                }
+                                Data.tns.SaveChanges();
+                                context.Response.StatusCode = 200;                               
+                            }
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                        context.Response.StatusCode = 400;
+                        context.Response.Close();
+                    }
+                }
+                else if (context.Request.HttpMethod == "DELETE")
+                {
+                    try
+                    {
+                        if (context.Request.QueryString.Count == 1)
+                        {
+                            if (context.Request.QueryString.Keys[0] == "number")
+                            {
+                                string number = Convert.ToString(context.Request.QueryString.Get(0));
+                                var currentAbonent = Data.tns.Abonents.FirstOrDefault(b => b.NumberAbonent == number);
+                                if (currentAbonent != null)
+                                {
+                                    Data.tns.Abonents.Remove(currentAbonent);
+                                    Data.tns.SaveChanges();
+                                    context.Response.StatusCode = 200;
+                                    context.Response.Close();
+                                }
+                            }
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                        context.Response.StatusCode = 400;
+                        context.Response.Close();
+                    }
+                }
+            }
+        }
+    }
+}
